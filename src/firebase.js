7ebb741eabd5c56d3b475b19d6c805dbcdd2ede8
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-05-17 19:47:11 +0500

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "tutorialfirebase-cee9a.firebaseapp.com",
+  projectId: "tutorialfirebase-cee9a",
+  storageBucket: "tutorialfirebase-cee9a.appspot.com",
+  messagingSenderId: "138621784180",
+  appId: "1:138621784180:web:771ec42180ec3b589e4121"
+};
+
+const app = initializeApp(firebaseConfig);
