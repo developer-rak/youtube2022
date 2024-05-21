b387f5618faf81d756a3465d0c1eb7b4ed0b7987
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-05-21 10:39:05 +0500

    added AuthContext and AuthReducer local storage for user

--- src/App.js
+++ src/App.js
@@ -3,36 +3,88 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+
+  const {currentUser} = useContext(AuthContext);
+
+  const RequiredAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
+
+  console.log(currentUser)
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+          <Route path="login" element={<Login />} />
+            <Route 
+              index 
+              element={
+                <RequiredAuth>
+                  <Home />
+                </RequiredAuth>
+              } 
+            />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
+              <Route 
+                index 
+                element={
+                  <RequiredAuth>
+                    <List />
+                  </RequiredAuth>
+                } 
+              />
+              <Route 
+                path=":userId" 
+                element={
+                  <RequiredAuth>
+                    <Single />
+                  </RequiredAuth>
+                } 
+              />
               <Route
                 path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
+                element={
+                  <RequiredAuth>
+                    <New inputs={userInputs} title="Add New User" />
+                  </RequiredAuth>
+                }
               />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+              <Route 
+                index 
+                element={
+                  <RequiredAuth>
+                    <List />
+                  </RequiredAuth>
+                } 
+              />
+              <Route 
+                path=":productId" 
+                element={
+                  <RequiredAuth>
+                    <Single />
+                  </RequiredAuth>
+                } 
+              />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={
+                  <RequiredAuth>
+                    <New inputs={productInputs} title="Add New Product" />
+                  </RequiredAuth>
+                }
               />
             </Route>
           </Route>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const {dispatch} = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -18,6 +21,7 @@ const Login = () => {
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        dispatch({type:"LOGIN", payload:user})
         navigate("/")
         // ...
       })
